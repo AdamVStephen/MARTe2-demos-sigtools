9cc8cd7a91e26e5c7e6c702a756060deeb02a0e5
Adam V Stephen(adam@jump) <adam.v.stephen@gmail.com>
2022-02-20 16:46:26 +0000

    Fix PATH for DIR typo

--- bin/m2-completion.bash
+++ bin/m2-completion.bash
@@ -7,20 +7,10 @@
 # REF : https://stackoverflow.com/questions/57426500/list-directories-at-a-specific-path-as-autocomplete-options-for-a-bash-script
 # The following stackoverflow answer falls down on the underscore functions not being recognised : TO FIX
 
-_configs_x()
-{
-	local current;
-	local base=${MARTe2_CONFIG_PATH}
-	_get_comp_words_by_ref current;
-	current="${base}"/$current
-	_filedir
-	COMPREPLY=("${COMPREPLY[@]#$base}")
-}
-#
-_configs()
+configs()
 {
 	local cfgs cur
-	cfgs=$(ls -1 ${MARTe2_CONFIG_PATH}/*.cfg | while read f; do echo $(basename $f); done)
+	cfgs=$(ls -1 ${MARTe2_CONFIG_DIR}/*.cfg | while read f; do echo $(basename $f); done)
 	cur=${COMP_WORDS[COMP_CWORD]}
 	COMPREPLY=( $(compgen -W "$cfgs" -- ${cur}) )
 	return 0
